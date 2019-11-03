24aaaa416a20cd2e8bb5b98fb46eeefa012deba1
Jishnu Bhattacharya <jishnuonline@gmail.com>
2019-11-03 17:15:47 +0400

    Introduced PiecewiseUnitRange and PiecewiseIncreasingStepRange

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # PiecewiseIncreasingRanges
 
-[![Build Status](https://travis-ci.org/jishnub/PiecewiseIncreasingRanges.jl.svg?branch=dev)](https://travis-ci.org/jishnub/PiecewiseIncreasingRanges.jl)
+[![Build Status](https://travis-ci.com/jishnub/PiecewiseIncreasingRanges.jl.svg?branch=dev)](https://travis-ci.com/jishnub/PiecewiseIncreasingRanges.jl)
 [![Coverage Status](https://coveralls.io/repos/simonster/PiecewiseIncreasingRanges.jl/badge.svg?branch=master)](https://coveralls.io/r/simonster/PiecewiseIncreasingRanges.jl?branch=master)
 
-PiecewiseIncreasingRanges.jl provides a PiecewiseIncreasingRange type that corresponds to a set of concatenated, monotonically increasing ranges. It implements indexing as well as `searchsortedfirst`, `searchsortedlast`, and a `findnearest` function. This type is useful for holding the potentially discontinuous timestamps corresponding to large quantities of sampled data, where creating and indexing a vector would be undesirably slow.
+PiecewiseIncreasingRanges.jl provides a `PiecewiseIncreasingRange` type that corresponds to a set of concatenated, monotonically increasing ranges. It implements indexing as well as `searchsortedfirst`, `searchsortedlast`, and a `findnearest` function. This type is useful for holding the potentially discontinuous timestamps corresponding to large quantities of sampled data, where creating and indexing a vector would be undesirably slow.
+
+There are two other types that are provided for convenience: `PiecewiseUnitRange` and `PiecewiseIncreasingStepRange`, which are concatenated `Unitrange`s and `StepRange`s respectively. These behave like aliases of `PiecewiseIncreasingRange{T,UnitRange{T},Nothing}` and `PiecewiseIncreasingRange{T,StepRange{T},Nothing}`, but have been implemented as separate types.
+
+
+An extra type `IncreasingStepRange` is introduced for type stability. This can be used to slice any of the piecewise ranges. This behaves exactly like a `StepRange` with the extra constraint that the `step` is guaranteed to be positive. The instability is only in the final return type in `getindex` which returns a `Union` instead of a concrete type, so the performance is almost identical because of [union-splitting](https://julialang.org/blog/2018/08/union-splitting). However `IncreasingStepRange` is included just in case it leads to more efficient code.
 
 ## Construction
 
-You can construct a PiecewiseIncreasingRange as below:
+You can construct a `PiecewiseIncreasingRange` as
 
 ```julia
-julia> PiecewiseIncreasingRange(StepRange{Int,Int}[0:5:15, 18:2:20])
-6-element PiecewiseIncreasingRanges.PiecewiseIncreasingRange{Int64,StepRange{Int64,Int64},Void}:
+julia> PiecewiseIncreasingRange([0:5:15, 18:2:20])
+6-element PiecewiseIncreasingRanges.PiecewiseIncreasingRange{Int64,StepRange{Int64,Int64},Nothing}:
   0
   5
  10
@@ -20,7 +25,7 @@ julia> PiecewiseIncreasingRange(StepRange{Int,Int}[0:5:15, 18:2:20])
  20
 ```
 
-PiecewiseIncreasingRanges also accept an optional divisor, which is divided out of all elements:
+`PiecewiseIncreasingRange`s also accept an optional divisor, which is divided out of all elements:
 
 ```julia
 julia> PiecewiseIncreasingRange(UnitRange{Int}[0:3, 15:16], 4)
@@ -31,6 +36,171 @@ julia> PiecewiseIncreasingRange(UnitRange{Int}[0:3, 15:16], 4)
  0.75
  3.75
  4.0
+```
+
+The types `PiecewiseUnitRange` and `PiecewiseIncreasingStepRange` can be constructed as 
+
+```julia
+julia> PiecewiseUnitRange([0:2, 5:7])
+6-element PiecewiseUnitRange{Int64,UnitRange{Int64}}:
+ 0
+ 1
+ 2
+ 5
+ 6
+ 7
+
+julia> PiecewiseIncreasingStepRange([0:2:2,5:3:11])
+5-element PiecewiseIncreasingStepRange{Int64,StepRange{Int64,Int64}}:
+  0
+  2
+  5
+  8
+ 11
+```
+
+## Slicing
+
+Slicing a `PiecewiseIncreasingRange` produces another `PiecewiseIncreasingRange` for ranges with positive steps. Otherwise it defaults to an array.
+
+```julia
+julia> p=PiecewiseIncreasingRange([0:4:20, 61:15:80])
+8-element PiecewiseIncreasingRange{Int64,StepRange{Int64,Int64},Nothing}:
+  0
+  4
+  8
+ 12
+ 16
+ 20
+ 61
+ 76
+
+julia> p[1:4]
+4-element PiecewiseIncreasingRange{Int64,StepRange{Int64,Int64},Nothing}:
+  0
+  4
+  8
+ 12
+
+julia> p[1:3:7] # not type stable as return type depends on the sign of the step
+3-element PiecewiseIncreasingRange{Int64,StepRange{Int64,Int64},Nothing}:
+  0
+ 12
+ 61
+
+julia> p[IncreasingStepRange(1:3:7)]  # type stable
+3-element PiecewiseIncreasingRange{Int64,StepRange{Int64,Int64},Nothing}:
+  0
+ 12
+ 61
+
+julia> p[4:-1:1]
+4-element Array{Int64,1}:
+ 12
+  8
+  4
+  0
+```
+
+Slicing a `PiecewiseUnitRange` produces a `PiecewiseUnitRange` if sliced with an `UnitRange`, a `PiecewiseIncreasingStepRange` if sliced with a `StepRange` or an `IncreasingStepRange`, and an array otherwise.
+
+```julia
+julia> p=PiecewiseUnitRange([1:3,6:7,10:12])
+8-element PiecewiseUnitRange{Int64,UnitRange{Int64}}:
+  1
+  2
+  3
+  6
+  7
+ 10
+ 11
+ 12
+
+julia> p[1:4]
+4-element PiecewiseUnitRange{Int64,UnitRange{Int64}}:
+ 1
+ 2
+ 3
+ 6
+
+julia> p[1:2:5] # not type stable as return type depends on the sign of the step
+3-element PiecewiseIncreasingStepRange{Int64,StepRange{Int64,Int64}}:
+ 1
+ 3
+ 7
+
+julia> p[IncreasingStepRange(1:2:5)] # type stable
+3-element PiecewiseIncreasingStepRange{Int64,StepRange{Int64,Int64}}:
+ 1
+ 3
+ 7
+
+julia> p[5:-2:1]
+3-element Array{Int64,1}:
+ 7
+ 3
+ 1
+```
+
+Slicing a `PiecewiseIncreasingStepRange` produces another `PiecewiseIncreasingStepRange` if the step size is positive, or an array otherwise. Type stability is achieved using `IncreasingStepRange`.
+
+```julia
+julia> p=PiecewiseIncreasingStepRange([1:2:5,10:4:18])
+6-element PiecewiseIncreasingStepRange{Int64,StepRange{Int64,Int64}}:
+  1
+  3
+  5
+ 10
+ 14
+ 18
+
+julia> p[1:4]
+4-element PiecewiseIncreasingStepRange{Int64,StepRange{Int64,Int64}}:
+  1
+  3
+  5
+ 10
+
+julia> p[1:2:5] # not type stable as return type depends on the sign of the step
+3-element PiecewiseIncreasingStepRange{Int64,StepRange{Int64,Int64}}:
+  1
+  5
+ 14
+
+julia> p[IncreasingStepRange(1:2:5)] # type stable
+3-element PiecewiseIncreasingStepRange{Int64,StepRange{Int64,Int64}}:
+  1
+  5
+ 14
+
+julia> p[5:-2:1]
+3-element Array{Int64,1}:
+ 14
+  5
+  1
+```
+
+## Searchsortedfirst and searchsortedlast
+
+These functions are implemented for all the types.
+
+```julia
+julia> p=PiecewiseUnitRange([1:3,6:7,10:12])
+8-element PiecewiseUnitRange{Int64,UnitRange{Int64}}:
+  1
+  2
+  3
+  6
+  7
+ 10
+ 11
+ 12
+
+julia> searchsortedfirst(p,4)
+4
+
+julia> searchsortedlast(p,4)
+3
 ```
 
 ## findnearest
--- src/PiecewiseIncreasingRanges.jl
+++ src/PiecewiseIncreasingRanges.jl
@@ -1,7 +1,47 @@
 module PiecewiseIncreasingRanges
-export PiecewiseIncreasingRange, NoNearestSampleError, findnearest, resample
+export PiecewiseIncreasingRange, NoNearestSampleError, findnearest, resample,
+PiecewiseUnitRange,PiecewiseIncreasingStepRange,IncreasingStepRange
 
 using Compat, Base.Order
+import Base: @propagate_inbounds
+
+struct IncreasingStepRange{T, S} <: OrdinalRange{T, S}
+    start :: T
+    step :: S
+    stop :: T
+
+    function IncreasingStepRange{T,S}(start,step,stop) where {T,S,R<:OrdinalRange}
+        step <= 0 && throw(DomainError(step,"step has to be positive"))
+        new{T,S}(start,step,stop)
+    end
+end
+
+IncreasingStepRange(start::T,step::S,stop::T) where {T<:Integer,S<:Integer} = IncreasingStepRange{T,S}(start,step,stop)
+IncreasingStepRange(r::OrdinalRange{T,S}) where {T<:Integer,S<:Integer} = IncreasingStepRange{T,S}(r.start,r.step,r.stop)
+
+Base.step(r::IncreasingStepRange) = r.step
+Base.isempty(r::IncreasingStepRange) =
+    (r.start != r.stop) & ((r.step > zero(r.step)) != (r.stop > r.start))
+
+function Base.length(r::IncreasingStepRange{T}) where T<:Union{Int,UInt,Int64,UInt64,Int128,UInt128}
+    isempty(r) && return zero(T)
+    if r.step > 1
+        return Base.checked_add(convert(T, div(unsigned(r.stop - r.start), r.step)), one(T))
+    else
+        return Base.checked_add(div(Base.checked_sub(r.stop, r.start), r.step), one(T))
+    end
+end
+Base.firstindex(::IncreasingStepRange) = 1
+@inline @propagate_inbounds function Base.getindex(r::IncreasingStepRange, 
+    s::AbstractRange{<:Integer})
+
+    @boundscheck checkbounds(r, s)
+    st = oftype(r.start, r.start + (first(s)-1)*step(r))
+    range(st, step=step(r)*step(s), length=length(s))
+end
+
+abstract type AbstractPiecewiseRange{T,R} <: AbstractVector{T} end
+abstract type AbstractPiecewiseOrdinalRange{T,R} <: AbstractPiecewiseRange{T,R} end
 
 constructrange(::Type{T}, start, step, stop) where {T<:UnitRange} = T(start, stop)
 constructrange(::Type{T}, start, step, stop) where {T<:StepRange} = T(start, step, stop)
@@ -74,7 +114,7 @@ function combine_ranges(ranges::Vector{R}, firstrg::R, firstrgidx::Int) where {R
     (newranges, offsets)
 end 
 
-struct PiecewiseIncreasingRange{T,R<:AbstractRange,S} <: AbstractVector{T}
+struct PiecewiseIncreasingRange{T,R<:AbstractRange,S} <: AbstractPiecewiseRange{T,R}
     ranges::Vector{R}
     offsets::Vector{Int}
     divisor::S
@@ -92,27 +132,155 @@ struct PiecewiseIncreasingRange{T,R<:AbstractRange,S} <: AbstractVector{T}
         new{T,R,S}(newranges, offsets, divisor)
     end
 end
-PiecewiseIncreasingRange(ranges::Vector{R}, divisor) where {R<:AbstractRange} = PiecewiseIncreasingRange{typeof(inv(one(eltype(R)))),R,typeof(divisor)}(ranges, divisor)
-PiecewiseIncreasingRange(ranges::Vector{R}) where {R<:AbstractRange} = PiecewiseIncreasingRange{eltype(R),R,@compat(Nothing)}(ranges, nothing)
+PiecewiseIncreasingRange(ranges::Vector{R}, divisor) where {R<:AbstractRange} = 
+    PiecewiseIncreasingRange{typeof(inv(one(eltype(R)))),R,typeof(divisor)}(ranges, divisor)
+PiecewiseIncreasingRange(ranges::Vector{R}) where {R<:AbstractRange} = 
+    PiecewiseIncreasingRange{eltype(R),R,@compat(Nothing)}(ranges, nothing)
+PiecewiseIncreasingRange{T,R,@compat(Nothing)}(ranges::Vector{R}) where {T,R<:AbstractRange} = 
+    PiecewiseIncreasingRange{T,R,@compat(Nothing)}(ranges, nothing)
 
-Base.convert(::Type{PiecewiseIncreasingRange{T,R,S}}, x::PiecewiseIncreasingRange{T,R,S}) where {T,R<:AbstractRange,S} = x
+# const PiecewiseUnitRange{T,R<:AbstractUnitRange} = PiecewiseIncreasingRange{T,R,@compat(Nothing)}
+struct PiecewiseUnitRange{T,R<:AbstractUnitRange} <: AbstractPiecewiseOrdinalRange{T,R}
+    ranges::Vector{R}
+    offsets::Vector{Int}
+
+    function PiecewiseUnitRange{T,R}(ranges::AbstractVector) where {T,R<:AbstractUnitRange}
+        ranges = convert(Vector{R}, ranges)
+        isempty(ranges) && return new{T,R}(ranges, Int[])
+
+        # Find first non-empty range
+        j = findfirst(!isempty,ranges)
+        isnothing(j) && return new{T,R}(R[], Int[])
+
+        firstrg = ranges[j]
+        newranges, offsets = combine_ranges(ranges, firstrg, j+1)
+        new{T,R}(newranges, offsets)
+    end
+end
+PiecewiseUnitRange(ranges::Vector{R}) where {R<:AbstractUnitRange} = 
+    PiecewiseUnitRange{eltype(R),R}(ranges)
+
+# const PiecewiseIncreasingStepRange{T,R<:StepRange} = PiecewiseIncreasingRange{T,R,@compat(Nothing)}
+struct PiecewiseIncreasingStepRange{T,R<:StepRange} <: AbstractPiecewiseOrdinalRange{T,R}
+    ranges::Vector{R}
+    offsets::Vector{Int}
+
+    function PiecewiseIncreasingStepRange{T,R}(ranges::AbstractVector) where {T,R<:StepRange}
+        ranges = convert(Vector{R}, ranges)
+        isempty(ranges) && return new{T,R}(ranges, Int[])
+
+        # Find first non-empty range
+        j = findfirst(!isempty,ranges)
+        isnothing(j) && return new{T,R}(R[], Int[])
+
+        firstrg = ranges[j]
+        newranges, offsets = combine_ranges(ranges, firstrg, j+1)
+        new{T,R}(newranges, offsets)
+    end
+end
+PiecewiseIncreasingStepRange(ranges::Vector{R}) where {R<:StepRange} = 
+    PiecewiseIncreasingStepRange{eltype(R),R}(ranges)
+function PiecewiseIncreasingStepRange(ranges::Vector{R}) where {R<:AbstractUnitRange}
+    T = eltype(R)
+    PiecewiseIncreasingStepRange{T,StepRange{T,T}}(ranges)
+end
+PiecewiseIncreasingStepRange(x::PiecewiseUnitRange) = PiecewiseIncreasingStepRange(x.ranges)
+
 Base.convert(::Type{PiecewiseIncreasingRange{T,R,S}}, x::PiecewiseIncreasingRange) where {T,R,S} =
     PiecewiseIncreasingRange{T,R,S}(x.ranges, x.divisor)
 
+function Base.convert(::Type{PiecewiseIncreasingStepRange{T,R}},x::PiecewiseUnitRange) where {T,R}
+    PiecewiseIncreasingStepRange{T,R}(x.ranges)
+end
+
 # Avoid applying the divisor if it is one, to get types right
-divide_divisor(r::PiecewiseIncreasingRange{T,R,@compat(Nothing)}, x) where {T,R} = x
-multiply_divisor(r::PiecewiseIncreasingRange{T,R,@compat(Nothing)}, x) where {T,R} = x
+divide_divisor(::PiecewiseIncreasingRange{T,R,@compat(Nothing)}, x) where {T,R} = x
+multiply_divisor(::PiecewiseIncreasingRange{T,R,@compat(Nothing)}, x) where {T,R} = x
 divide_divisor(r::PiecewiseIncreasingRange{T,R,S}, x) where {T,R,S} = x/r.divisor
 multiply_divisor(r::PiecewiseIncreasingRange{T,R,S}, x) where {T,R,S} = x*r.divisor
 
-function Base.size(r::PiecewiseIncreasingRange)
+divide_divisor(::AbstractPiecewiseOrdinalRange, x) = x
+multiply_divisor(::AbstractPiecewiseOrdinalRange, x) = x
+
+function Base.size(r::AbstractPiecewiseRange)
     isempty(r.ranges) && return (0,)
     return (r.offsets[end]+length(r.ranges[end])-1,)
 end
 
-function Base.getindex(r::PiecewiseIncreasingRange, i::Integer)
+function Base.getindex(r::AbstractPiecewiseRange, i::Integer)
     rgidx = searchsortedlast(r.offsets, i, Forward)
     divide_divisor(r, r.ranges[rgidx][i-r.offsets[rgidx]+1])
+end
+
+@inline _inds(r,x::OrdinalRange,ind,elmax) = first(x) - r.offsets[ind]+1:step(x):elmax
+@inline _inds(r,x::AbstractUnitRange,ind,elmax) = first(x)-r.offsets[ind]+1:elmax
+
+@inline function _newrgx(r,x,ind)
+    elmax = min(last(x)-r.offsets[ind]+1, length(r.ranges[ind]))
+    newrg = r.ranges[ind][_inds(r,x,ind,elmax)]
+    x = x[length(newrg)+1:end]
+    return newrg,x
+end
+
+@inline function _fill_newrgs!(newrgs,r,x,firstrgidx,lastrgidx)
+    for irange in firstrgidx:lastrgidx
+        newrg,x = _newrgx(r,x,irange)
+        newrgs[irange-firstrgidx+1] = newrg
+    end
+    return newrgs
+end
+
+@inline function _compute_newrgs(Tret::Type,r::AbstractPiecewiseRange,x)
+    firstrgidx = searchsortedlast(r.offsets, first(x), Forward)
+    lastrgidx = searchsortedlast(r.offsets, last(x), Forward)
+    newrgs = Vector{Tret}(undef, lastrgidx-firstrgidx+1)
+    _fill_newrgs!(newrgs,r,x,firstrgidx,lastrgidx)
+end
+
+@inline function _getindex(r::AbstractPiecewiseRange{T,R},
+    x::AbstractUnitRange{<:Integer}) where {T,R}
+    _compute_newrgs(R,r,x)
+end
+
+# These methods are needed to avoid ambiguities
+@inline function _getindex(r::AbstractPiecewiseRange{T,R},
+    x::AbstractUnitRange{<:Integer}) where {T,R<:Union{OrdinalRange,StepRangeLen}}
+    _compute_newrgs(R,r,x)
+end
+
+@inline function _getindex(r::AbstractPiecewiseRange{T,R},
+    x::AbstractUnitRange{<:Integer}) where {T,R<:AbstractUnitRange}
+    _compute_newrgs(R,r,x)
+end
+
+# Slicing a Rational StepRange produces a StepRangeLen, so need to handle this separately
+@inline function _getindex(r::AbstractPiecewiseRange{Rational{T},StepRange{Rational{T},Rational{T}}},
+    x::AbstractUnitRange{<:Integer}) where {T<:Integer}
+
+    Tret = StepRangeLen{Rational{T},Rational{T}}
+    _compute_newrgs(Tret,r,x)
+end
+
+@inline function _getindex(r::AbstractPiecewiseRange{Rational{T},StepRange{Rational{T},Rational{T}}},
+    x::OrdinalRange{<:Integer}) where {T<:Integer}
+
+    Tret = StepRangeLen{Rational{T},Rational{T}}
+    _compute_newrgs(Tret,r,x)
+end
+
+# Slicing an AbsractUnitRange using a StepRange produces another StepRange
+@inline function _getindex(r::AbstractPiecewiseRange{T,R},
+    x::OrdinalRange{<:Integer}) where {T,R<:AbstractUnitRange}
+
+    Tret = StepRange{eltype(R),eltype(R)}
+    _compute_newrgs(Tret,r,x)
+end
+
+# Slicing an integral StepRange or a StepRangeLen produces the same type
+@inline function _getindex(r::AbstractPiecewiseRange{T,R},
+    x::OrdinalRange{<:Integer}) where {T,R<:Union{OrdinalRange,StepRangeLen}}
+
+    _compute_newrgs(R,r,x)
 end
 
 function Base.getindex(r::PiecewiseIncreasingRange{T,R,S}, x::AbstractRange{Int}) where {T,R,S}
@@ -120,18 +288,65 @@ function Base.getindex(r::PiecewiseIncreasingRange{T,R,S}, x::AbstractRange{Int}
     (first(x) >= 1 && last(x) <= length(r)) || throw(BoundsError())
 
     # Not type-stable!
-    step(x) < 0 && return invoke(getindex, (typeof(r), AbstractVector{Int}), r, x)
-
-    firstrgidx = searchsortedlast(r.offsets, first(x), Forward)
-    lastrgidx = searchsortedlast(r.offsets, last(x), Forward)
-    newrgs = Array{R}(undef, lastrgidx-firstrgidx+1)
-    for irange = firstrgidx:lastrgidx
-        elmax = min(last(x)-r.offsets[irange]+1, length(r.ranges[irange]))
-        newrg = newrgs[irange-firstrgidx+1] = r.ranges[irange][first(x)-r.offsets[irange]+1:elmax]
-        x = x[length(newrg)+1:end]
+    if step(x) < 0
+        argtype = Tuple{PiecewiseIncreasingRange{T,R,S}, AbstractVector{Int}}
+        return invoke(getindex, argtype, r, x)
     end
-    PiecewiseIncreasingRange{T,R,S}(newrgs, r.divisor)
+
+    newrgs = _getindex(r,x)
+    PiecewiseIncreasingRange{T,eltype(newrgs),S}(newrgs, r.divisor)
 end
+
+function Base.getindex(r::PiecewiseUnitRange{T,R}, x::AbstractUnitRange{Int}) where {T,R}
+    isempty(x) && return PiecewiseUnitRange{T,R}(R[], Int[])
+    (first(x) >= 1 && last(x) <= length(r)) || throw(BoundsError())
+
+    PiecewiseUnitRange(_getindex(r,x))
+end
+
+@inline @propagate_inbounds function Base.getindex(r::PiecewiseUnitRange{T,R}, x::IncreasingStepRange{Int,Int}) where {T,R}
+    isempty(x) && return PiecewiseIncreasingStepRange{T,R}(R[], Int[])
+    (first(x) >= 1 && last(x) <= length(r)) || throw(BoundsError())
+
+    PiecewiseIncreasingStepRange(_getindex(r,x))
+end
+
+@inline @propagate_inbounds function Base.getindex(r::PiecewiseUnitRange{T,R}, x::StepRange{Int,Int}) where {T,R}
+    isempty(x) && return PiecewiseIncreasingStepRange{T,R}(R[], Int[])
+    (first(x) >= 1 && last(x) <= length(r)) || throw(BoundsError())
+
+    # Not type-stable!
+    if step(x) < 0 
+        argtype = Tuple{PiecewiseUnitRange{T,R}, AbstractVector{Int}}
+        return invoke(getindex, argtype , r, x)
+    end
+
+    PiecewiseIncreasingStepRange(_getindex(r,x))
+end
+
+@inline @propagate_inbounds function Base.getindex(r::PiecewiseIncreasingStepRange{T,R}, x::StepRange{Int,Int}) where {T,R}
+    isempty(x) && return PiecewiseIncreasingStepRange{T,R}(R[], Int[])
+    (first(x) >= 1 && last(x) <= length(r)) || throw(BoundsError())
+
+    # Not type-stable!
+    if step(x) < 0 
+        argtype = Tuple{PiecewiseIncreasingStepRange{T,R}, AbstractVector{Int}}
+        return invoke(getindex, argtype , r, x)
+    end
+
+    PiecewiseIncreasingStepRange(_getindex(r,x))
+end
+
+@inline @propagate_inbounds function Base.getindex(r::PiecewiseIncreasingStepRange{T,R}, 
+    x::Union{AbstractUnitRange{Int},IncreasingStepRange{Int,Int}}) where {T,R}
+    
+    isempty(x) && return PiecewiseIncreasingStepRange{T,R}(R[], Int[])
+    (first(x) >= 1 && last(x) <= length(r)) || throw(BoundsError())
+
+    PiecewiseIncreasingStepRange(_getindex(r,x))
+end
+
+@inline @propagate_inbounds Base.getindex(r::AbstractPiecewiseRange,::Colon) = r
 
 function resample(r::PiecewiseIncreasingRange{T,R,S}, ratio::Rational{Int}) where {T,R,S}
     excess = zero(eltype(R))
@@ -168,34 +383,31 @@ Base.Order.lt(o::PiecewiseIncreasingRangeFirstOrdering, a, b) = isless(first(a),
 struct PiecewiseIncreasingRangeLastOrdering <: Ordering end
 Base.Order.lt(o::PiecewiseIncreasingRangeLastOrdering, a, b) = isless(last(a), last(b))
 
-const PIRLO = PiecewiseIncreasingRangeLastOrdering()
-const PIRFO = PiecewiseIncreasingRangeFirstOrdering()
-
-function Base.searchsortedfirst(r::PiecewiseIncreasingRange, x)
+function Base.searchsortedfirst(r::AbstractPiecewiseRange, x)
     isempty(r.ranges) && return 1
     xd = multiply_divisor(r, x)
 
-    rgidx = searchsortedfirst(r.ranges, xd, PIRLO)
+    rgidx = searchsortedfirst(r.ranges, xd, PiecewiseIncreasingRangeLastOrdering())
     rgidx > length(r.ranges) && return length(r) + 1
     searchsortedfirst(r.ranges[rgidx], xd, Forward) + r.offsets[rgidx] - 1
 end
 
-function Base.searchsortedlast(r::PiecewiseIncreasingRange, x)
+function Base.searchsortedlast(r::AbstractPiecewiseRange, x)
     isempty(r.ranges) && return 1
     xd = multiply_divisor(r, x)
 
-    rgidx = searchsortedlast(r.ranges, xd, PIRFO)
+    rgidx = searchsortedlast(r.ranges, xd, PiecewiseIncreasingRangeFirstOrdering())
     rgidx == 0 && return 0
     searchsortedlast(r.ranges[rgidx], xd, Forward) + r.offsets[rgidx] - 1
 end
 
 struct NoNearestSampleError <: Exception end
 
-function findnearest(r::PiecewiseIncreasingRange, x, within_half_step::Bool=false)
+function findnearest(r::AbstractPiecewiseRange, x, within_half_step::Bool=false)
     isempty(r.ranges) && throw(NoNearestSampleError())
     xd = multiply_divisor(r, x)
 
-    rgidx = searchsortedfirst(r.ranges, xd, PIRLO)
+    rgidx = searchsortedfirst(r.ranges, xd, PiecewiseIncreasingRangeLastOrdering())
     if rgidx > length(r.ranges)
         rgend = r.ranges[end]
         within_half_step && xd > rgend[end]+step(rgend)/2 && throw(NoNearestSampleError())
@@ -225,24 +437,25 @@ function findnearest(r::PiecewiseIncreasingRange, x, within_half_step::Bool=fals
 end
 
 # These functions let us use PiecewiseIncreasingRange as an array axis
-Base.checkindex(::Type{Bool}, inds::PiecewiseIncreasingRange, i) =
+Base.checkindex(::Type{Bool}, inds::AbstractPiecewiseRange, i) =
     throw(ArgumentError("unable to check bounds for indices of type $(typeof(i))"))
-function Base.checkindex(::Type{Bool},inds::PiecewiseIncreasingRange,i::Real)
-    found=false
+
+function Base.checkindex(::Type{Bool},inds::AbstractPiecewiseRange,i::Real)
+    inbound=false
     for r in inds.ranges
-        found |= i ∈ r
-        found && break
+        inbound |= divide_divisor(inds,first(r)) <= i <= divide_divisor(inds,last(r))
+        inbound && break
     end
-    found
+    inbound
 end
-Base.checkindex(::Type{Bool},inds::PiecewiseIncreasingRange,::Colon) = true
-Base.checkindex(::Type{Bool},inds::PiecewiseIncreasingRange,::Base.Slice) = true
-function Base.checkindex(::Type{Bool}, inds::PiecewiseIncreasingRange, r::AbstractRange)
-    isempty(r) | any(checkindex(Bool,indrng,r) for indrng in inds.ranges)
+
+Base.checkindex(::Type{Bool},inds::AbstractPiecewiseRange,::Colon) = true
+Base.checkindex(::Type{Bool},inds::AbstractPiecewiseRange,::Base.Slice) = true
+function Base.checkindex(::Type{Bool}, indx::AbstractPiecewiseRange, I::AbstractVector{Bool}) 
+    indx == Base.axes1(parent(I))
 end
-Base.checkindex(::Type{Bool}, indx::PiecewiseIncreasingRange, I::AbstractVector{Bool}) = UnitRange(indx) == axes(parent(I),1)
-Base.checkindex(::Type{Bool}, indx::PiecewiseIncreasingRange, I::AbstractArray{Bool}) = false
-function Base.checkindex(::Type{Bool}, inds::PiecewiseIncreasingRange, I::AbstractArray)
+Base.checkindex(::Type{Bool}, indx::AbstractPiecewiseRange, I::AbstractArray{Bool}) = false
+function Base.checkindex(::Type{Bool}, inds::AbstractPiecewiseRange, I::AbstractVector)
     b = true
     for i in I
         b &= checkindex(Bool, inds, i)
